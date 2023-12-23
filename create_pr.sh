d618589c890ebb5d34a7cfb3409e1c14744358e7
devo1ution <danielfangzhu@gmail.com>
2023-12-23 14:37:21 -0800

    add github actions credentials

--- create_pr.sh
+++ create_pr.sh
@@ -1,3 +1,5 @@
+git config --global user.name github-actions[bot]
+git config --global user.email github-actions[bot]@users.noreply.github.com
 git add .
 git commit -m "auto-updates"
 gh pr create -B main -H auto --title 'Problem auto-updates' --body 'Created by GitHub Actions'
