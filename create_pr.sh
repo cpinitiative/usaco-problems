6f45d73c3885db74b0317ca3e533b251c2333f14
devo1ution <danielfangzhu@gmail.com>
2023-12-23 14:46:57 -0800

    this should work?

--- create_pr.sh
+++ create_pr.sh
@@ -2,5 +2,5 @@ git config --global user.name github-actions[bot]
 git config --global user.email github-actions[bot]@users.noreply.github.com
 git add .
 git commit -m "auto-updates"
-git push
+git push --set-upstream origin auto
 gh pr create --title 'Problem auto-updates' --body 'Created by GitHub Actions'
