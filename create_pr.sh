c11481c9e1cd86fbffdaea57b92d293e5dfa1fcd
devo1ution <danielfangzhu@gmail.com>
2023-12-23 15:04:23 -0800

    have auto force push

--- create_pr.sh
+++ create_pr.sh
@@ -2,5 +2,5 @@ git config --global user.name github-actions[bot]
 git config --global user.email github-actions[bot]@users.noreply.github.com
 git add .
 git commit -m "auto-updates"
-git push --set-upstream origin auto
+git push -f origin auto
 gh pr create --title 'Problem auto-updates' --F out/report.txt
