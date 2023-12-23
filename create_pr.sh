d1d6bbb446e295bd6bab1a56719f23123fe8fd63
devo1ution <danielfangzhu@gmail.com>
2023-12-23 15:05:24 -0800

    oops typo

--- create_pr.sh
+++ create_pr.sh
@@ -3,4 +3,4 @@ git config --global user.email github-actions[bot]@users.noreply.github.com
 git add .
 git commit -m "auto-updates"
 git push -f origin auto
-gh pr create --title 'Problem auto-updates' --F out/report.txt
+gh pr create --title 'Problem auto-updates' -F out/report.txt
