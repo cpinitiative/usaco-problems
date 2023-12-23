82a17f4ae3da34a7007a0057bd57ce9b615f92a1
devo1ution <danielfangzhu@gmail.com>
2023-12-23 14:44:12 -0800

    update create_pr

--- create_pr.sh
+++ create_pr.sh
@@ -2,4 +2,5 @@ git config --global user.name github-actions[bot]
 git config --global user.email github-actions[bot]@users.noreply.github.com
 git add .
 git commit -m "auto-updates"
-gh pr create -B main -H auto --title 'Problem auto-updates' --body 'Created by GitHub Actions'
+git push
+gh pr create --title 'Problem auto-updates' --body 'Created by GitHub Actions'
