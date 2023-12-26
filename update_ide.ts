c39e9695c7c2f5f836aa5127a9b6630f15713172
devo1ution <danielfangzhu@gmail.com>
2023-12-25 21:44:00 -0800

    throw error

--- update_ide.ts
+++ update_ide.ts
@@ -1,5 +1,6 @@
 import algoliasearch from 'algoliasearch';
 import { ProblemData } from './ProblemData';
+if (!process.env.ALGOLIA_API_KEY) throw new Error('ALGOLIA_API_KEY not set');
 const client = algoliasearch('3CFULMFIDW', process.env.ALGOLIA_API_KEY);
 const index = client.initIndex('usacoProblems');
 index.replaceAllObjects(
