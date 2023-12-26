a346e780a08ee12de088af987d289abe03f941bb
devo1ution <danielfangzhu@gmail.com>
2023-12-25 21:42:58 -0800

    use env variable instead

--- update_ide.ts
+++ update_ide.ts
@@ -1,6 +1,6 @@
 import algoliasearch from 'algoliasearch';
 import { ProblemData } from './ProblemData';
-const client = algoliasearch('3CFULMFIDW', process.argv[2]);
+const client = algoliasearch('3CFULMFIDW', process.env.ALGOLIA_API_KEY);
 const index = client.initIndex('usacoProblems');
 index.replaceAllObjects(
   Object.values(require('./problems.json') as {[key: string]: ProblemData}).map(x => ({
