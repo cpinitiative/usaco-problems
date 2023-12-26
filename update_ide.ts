82c86e930d437ef4e42e3d6a090840e74306d159
devo1ution <danielfangzhu@gmail.com>
2023-12-25 20:49:48 -0800

    forgot some stuff

--- update_ide.ts
+++ update_ide.ts
@@ -1,4 +1,11 @@
 import algoliasearch from 'algoliasearch';
+import { ProblemData } from './ProblemData';
 const client = algoliasearch('3CFULMFIDW', process.env.ALGOLIA_API_KEY ?? '');
 const index = client.initIndex('usacoProblems');
-index.replaceAllObjects(require('./problems.json'));
+index.replaceAllObjects(
+  Object.values(require('./problems.json') as {[key: string]: ProblemData}).map(x => ({
+    ...x,
+    title: x.title.titleString,
+    source: x.source.sourceString
+  }))
+);
